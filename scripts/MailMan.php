5856da6bd2ae45564f2e5d970cb8219aa96e506f
Patrick Joel <joekenpat@gmail.com>
2021-08-05 06:50:41 +0100

    updated mail template content

--- scripts/MailMan.php
+++ scripts/MailMan.php
@@ -11,43 +11,43 @@ require '../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable("../");
 $dotenv->load();
-$dotenv->required(['MAIL_HOST', 'MAIL_PORT', 'MAIL_USERNAME', 'MAIL_PASSWORD','FROM_EMAIL','FROM_NAME']);
+$dotenv->required(['MAIL_HOST', 'MAIL_PORT', 'MAIL_USERNAME', 'MAIL_PASSWORD', 'FROM_EMAIL', 'FROM_NAME']);
 
 function SendWelcomeMail($from, $to, $subject = "Welcome to Qwiki!")
 {
-    //Create an instance; passing `true` enables exception
-    try {
-        $mail = new PHPMailer(true);
-        $mail->CharSet = "UTF-8";
-        $mail->Host = $_ENV["MAIL_HOST"];
-        $mail->isSMTP();
-        $mail->SMTPAuth = true;
-        $mail->Username = $_ENV["MAIL_USERNAME"];
-        $mail->Password = $_ENV["MAIL_PASSWORD"];
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = $_ENV["MAIL_PORT"];
-        // $mail->SMTPDebug = 3;
-        $mail->setFrom($_ENV["FROM_EMAIL"], $_ENV["FROM_NAME"]);
-        $mail->addAddress($to["email"], $to["name"]);
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $user_name  = urlencode($to["name"]);
-        $mail->msgHTML(
-            file_get_contents(
-                ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://')
-                    . $_SERVER['HTTP_HOST']
-                    . dirname($_SERVER['PHP_SELF'])
-                    . "/welcome_mail_template.php?user_name={$user_name}"
-            ),
-            __DIR__
-        );
-        $mail->send();
-        http_response_code(200);
-        header('Content-Type: application/json');
-        echo json_encode(["status" => "success", "message" => "Mail Sent"]);
-    } catch (Exception $e) {
-        http_response_code(500);
-        header('Content-Type: application/json');
-        echo json_encode(["status" => "error", "message" => "Mailer Error: {$mail->ErrorInfo}"]);
-    }
+  //Create an instance; passing `true` enables exception
+  try {
+    $mail = new PHPMailer(true);
+    $mail->CharSet = "UTF-8";
+    $mail->Host = $_ENV["MAIL_HOST"];
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV["MAIL_USERNAME"];
+    $mail->Password = $_ENV["MAIL_PASSWORD"];
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = $_ENV["MAIL_PORT"];
+    // $mail->SMTPDebug = 3;
+    $mail->setFrom($_ENV["FROM_EMAIL"], $_ENV["FROM_NAME"]);
+    $mail->addAddress($to["email"], $to["name"]);
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $user_name  = urlencode($to["name"]);
+    $mail->msgHTML(
+      file_get_contents(
+        ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://')
+          . $_SERVER['HTTP_HOST']
+          . dirname($_SERVER['PHP_SELF'])
+          . "/welcome_mail_template.php?user_name={$user_name}"
+      ),
+      __DIR__
+    );
+    $mail->send();
+    http_response_code(200);
+    header('Content-Type: application/json');
+    echo json_encode(["status" => "success", "message" => "Mail Sent"]);
+  } catch (Exception $e) {
+    http_response_code(500);
+    header('Content-Type: application/json');
+    echo json_encode(["status" => "error", "message" => "Mailer Error: {$mail->ErrorInfo}"]);
+  }
 }
